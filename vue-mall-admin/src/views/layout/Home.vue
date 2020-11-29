2c35c15f1387c37d99b7228c7412c4aafc90ec3b
yyxyangyuanxin <1571827357@qq.com>
2020-11-29 23:46:28 +0800

    搭建项目、配置文件、菜单样式、头部样式

---
+++ vue-mall-admin/src/views/layout/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="wrapper">
+    <left-menu />
+    <sider-nav />
+  </div>
+</template>
+<script>
+import leftMenu from './component/menu.vue';
+import siderNav from './component/siderNav.vue'
+export default {
+  components: {
+    leftMenu,
+    siderNav
+  },
+};
+</script>
+
+<style lang='less' scoped>
+@import url('~@/styles/home/index.less');
+</style>
---
+++ vue-mall-admin/src/views/layout/component/siderNav.vue
@@ -0,0 +1,75 @@
+<template>
+  <div :class="{'wrapper': true, 'max-width': $store.state.collapsed}">
+    <a-button type="primary" @click="toggleCollapsed">
+      <a-icon :type="collapsed ? 'menu-unfold' : 'menu-fold'" />
+    </a-button>
+    <a-page-header :breadcrumb="{ props: { routes } }" />
+    <div class="user-info">
+      <div class="user-info-item">
+        欢迎yangyuanxin
+        <div class="exit-btn">退出</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      collapsed: false,
+      routes: [
+        {
+          path: "index",
+          breadcrumbName: "商品",
+        },
+        {
+          path: "first",
+          breadcrumbName: "商品列表",
+        },
+      ],
+    };
+  },
+  methods: {
+    toggleCollapsed() {
+      this.$store.state.collapsed = !this.$store.state.collapsed;
+    },
+  }
+};
+</script>
+
+<style lang='less' scoped>
+.wrapper {
+  width: calc(100vw - 180px);
+  border-bottom: 1px #ececec solid;
+  height: 50px;
+  line-height: 50px;
+  position: fixed;
+  top: 0;
+  right: 0;
+  &.max-width{
+    width: calc(100vw - 64px);
+  }
+  .ant-page-header {
+    float: left;
+    &.has-breadcrumb {
+      padding-top: 14px;
+    }
+  }
+  .user-info {
+    position: fixed;
+    right: 30px;
+    .user-info-item {
+      cursor: pointer;
+      .exit-btn {
+        display: none;
+        width: 100%;
+        text-align: center;
+      }
+      &:hover .exit-btn {
+        display: block;
+      }
+    }
+  }
+}
+</style>
